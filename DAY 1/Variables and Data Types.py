6ccfae6c29680fc495d95102c5f489fd878d9e92
sparshchopra11 <sparshchopra111111@gmail.com>
2026-03-22 13:09:06 +0530

    Day X: what you did

--- DAY 1/Variables and Data Types.py
+++ DAY 1/Variables and Data Types.py
@@ -12,3 +12,9 @@ print(int(float(f)))
 
 aa , bb = input("please enter 2 numbers to add :---> ").split()
 print("the final answer is :-> " + str(int(aa) + int(bb)))
+if (int(aa) > int(bb)) :
+    print("the first number " + aa + " is bigger than the second number")
+else :
+    print("the second number " + bb + " is bigger than the first number")
+cc = float(aa) % float(bb)
+print("the remainder we get when we divide the first number with the second number is " + str(cc))
