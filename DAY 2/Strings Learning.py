fe8458903e4520056e0a8c9b80db2bf389ec5c8b
sparshchopra11 <sparshchopra111111@gmail.com>
2026-03-29 21:54:01 +0530

    Day 3:Finished revising all done before

---
+++ DAY 2/Strings Learning.py
@@ -0,0 +1,10 @@
+# started to learn how to slice the strings in 
+
+first_name = "Heloooooo"
+short_name = first_name[0:3]
+import pyttsx3
+pyttsx3.speak(first_name)
+pyttsx3.speak("the shorter name is ")
+print(short_name)
+pyttsx3.speak(short_name)
+
---
+++ DAY 3/quick revision.py
@@ -0,0 +1,10 @@
+
+a = float(input("please enter the first number only :-->  " ))
+print("the number's square is :->  " + str(a*a))
+aa , bb = input("please enter the first and second number to add them both :-->  ").split()
+print("the sum of both the numbers are :-->  " + str(float(aa) + float(bb)))
+print("the sum of both the numbers are :-->  " + (aa+bb)) #just trying to see all possible happening
+
+b = "hello My name is Sparsh"
+c = b[0:8]
+print(c)
