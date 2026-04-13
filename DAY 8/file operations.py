05440f038f53b55068d53a896389c8a235e9ecf6
sparshchopra11 <sparshchopra111111@gmail.com>
2026-04-14 01:51:38 +0530

    DAY 8 : Practicing different questions on the file I/O operations

--- DAY 8/file operations.py
+++ DAY 8/file operations.py
@@ -1,4 +1,8 @@
 """
+
+#Write a program to read the text from a given file poems.txt and find out 
+#whether it contains the word twinkle.
+
 file1 = open("F:\\Learning\\AI ML Learn from Scratch\\DAY 8\\poems.txt" , "r")
 print("this program will tell whether the word Twinkle is there in the file or not ")
 word = "twinkle"
@@ -14,7 +18,10 @@ if(found is False):
     print("the twinkle was not there in the file ")
 
     
-
+#The game() function in a program lets a user play a game and returns the score 
+#as an integer. You need to read a file ‘Hi-score.txt’ which is either blank or 
+#contains the previous Hi-score. You need to write a program to update the Hi
+#score whenever the game() function breaks the Hi-score.
 
 import random 
 def game():
@@ -40,21 +47,47 @@ else:
 file.close()
 
 
-
-
-"""
+#Write a program to generate multiplication tables from 2 to 20 and write it to the 
+#different files. Place these files in a folder for a 13 – year old.
 
 valid = False
 while (valid is False):
     num = input("please enter the number of table you want to get in files starting from 1 :--> ")
-    if(num<0 or type(num) is str):
-        print("Please enter a valid input")
+    if int(num) is ValueError:
+        print("please enter a valid input")
     else:
         num = int(num)
         valid = True
+for i in range(1,num+1):
+    print("Creating the file number " + str(i))
+    file = open("F:\\Learning\\AI ML Learn from Scratch\\DAY 8\\" + str(i) + " Table.txt" , "a")
+    for j in range(1,11):
+        file.write(str(i) + " * " + str(j) + " = " + str(i*j) + "\n")
+    print("done the table of " + str(i) + " had been written in the file ")
+    file.close()
 
 
-for i in range(0,num):
-    print("Creating the file number " + str(i+1))
-    file = open("F:\\Learning\\AI ML Learn from Scratch\\DAY 8\\" + str(i+1) + " Table.txt" , "w")
     
+#A file contains a word “Donkey” multiple times. You need to write a program 
+#which replace this word with ##### by updating the same file.  
+try:
+    file = open("F:\\Learning\\AI ML Learn from Scratch\\DAY 8\\file_donkey.txt" , "r+")
+except FileNotFoundError:
+    print("the file dosen't exist in the computer")
+    exit()
+words = file.read()
+words = words.lower()
+word = "donkey"
+changed = 0
+words = words.replace(word , "######")
+file.seek(0)
+file.truncate()
+file.write(words)
+print("Done")
+
+
+
+
+"""
+
+
